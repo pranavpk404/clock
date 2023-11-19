ef4d0a39719feedcfb3b054f13438e51ee8642e1
Pranav <pranavpk404@gmail.com>
2023-11-19 10:36:57 +0000

    new api form

--- js/index.js
+++ js/index.js
@@ -1,47 +1,48 @@
-let text = document.getElementById("quote");
-let author = document.getElementById("author");
+let text = document.getElementById('quote')
+let author = document.getElementById('author')
 
 const startTime = () => {
-  let today = new Date();
-  let time = today.toLocaleTimeString();
-  document.getElementById("time-display").innerHTML = time;
+  let today = new Date()
+  let time = today.toLocaleTimeString()
+  document.getElementById('time-display').innerHTML = time
   const t = setTimeout(() => {
-    startTime();
-  }, 1000);
-};
+    startTime()
+  }, 1000)
+}
 
 const startDate = () => {
-  let today = new Date();
-  let timeAndDate = today.toLocaleDateString();
+  let today = new Date()
+  let timeAndDate = today.toLocaleDateString()
   let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let date = timeAndDate.slice(0, 10);
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ]
+  let date = timeAndDate.slice(0, 10)
   document.getElementById(`date`).innerHTML = `${
     days[today.getDay()]
-  } | ${date}`;
-};
+  } | ${date}`
+}
 
 const getNewQuote = async () => {
-  const url = `https://type.fit/api/quotes`;
-  const response = await fetch(url);
-  const allQuotes = await response.json();
-  const indx = Math.floor(Math.random() * allQuotes.length);
-  const quote = allQuotes[indx].text;
-  const auth = allQuotes[indx].author;
-  if (auth == null) {
-    author = `Anonymous`;
+  const url = `https://type.fit/api/quotes`
+  const response = await fetch(url)
+  const allQuotes = await response.json()
+  const indx = Math.floor(Math.random() * allQuotes.length)
+  const quote = allQuotes[indx].text
+  let auth = allQuotes[indx].author
+  auth = auth.split(',')[0]
+  if (auth == null || auth === 'type.fit') {
+    auth = `Anonymous`
   }
   if (quote.length > 80) {
-    text.style.fontSize = `20px`;
+    text.style.fontSize = `20px`
   }
-  document.getElementById("quote").innerHTML = quote;
-  document.getElementById("author").innerHTML = `~ ${auth}`;
-};
-getNewQuote();
+  document.getElementById('quote').innerHTML = quote
+  document.getElementById('author').innerHTML = `~ ${auth}`
+}
+getNewQuote()
